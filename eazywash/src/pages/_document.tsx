149a6573b14dc6a104092a9672c148d5ee260c9e
Sebastian Ledung <s.ledung@gmail.com>
2023-03-29 15:33:05 +0200

    Fixade grunden för att migrera designen till Bootstrap

--- eazywash/src/pages/_document.tsx
+++ eazywash/src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
         <link href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossOrigin="anonymous"></link>
       </Head>
       <body>
         <Main />
--- eazywash/src/pages/index.tsx
+++ eazywash/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '@/styles/index.module.css'
 
 export default function Index() {
@@ -11,39 +12,54 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossOrigin="anonymous" async></script>
       </Head>
-      <main className={styles.main}>
-      <header>
-        <a href="#"className="logo"><i className="bx bxs-washer" /><span>EazyWash</span></a>
-        <ul className="navbar">
-            <li><a href="#" className="active">Home</a></li>
-            <li><a href="#">Boka tid</a></li>
-            <li><a href="#">Tips</a></li>
-            <li><a href="#">Kontakta oss</a></li>
-        </ul>
-        <div className="main">
-            <a href="#" className="user"><i className="ri-user-fill"></i>Logga In</a>
-            <a href="#">Registera</a>
-            <div className="bx bx-menu"id="menu-icon"></div>            
-        </div>
-     </header>
-        <div className="videoBackground">
-            <video autoPlay={true} loop muted className="back-video">
-                <source src="laundry-basics.mp4" type="video/mp4" />
-          </video>
-        </div>
 
-        <div className="tips-box">
-            <div className="divbox">
-              <h2>This is the content of the first div box</h2>
-              <p>Here is some text for the first div box</p>
-            </div>
-            <div className="divbox">
-              <h2>This is the content of the second div box</h2>
-              <p>Here is some text for the second div box</p>
-            </div>
-          </div>
+      <main className={styles.main}>
+          <header className='container-fluid header'>            
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="#"><i className="bx bxs-washer"></i>EazyWash</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <a className="nav-link active" aria-current="page" href="#">Boka tvättid</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Felanmälan</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Om oss</a>
+                    </li>
+                  </ul>
+                  <ul className="navbar-nav mb-2 mb-lg-0"> 
+                      <li className="nav-item">
+                      <a className="nav-link" href="#"><i className="ri-user-fill"></i>Logga in</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Registrera</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </header>
       </main>
+
+
+      <div className="container-xl content">
+        <Image src="/xp.png" width={1000} height={1000} alt="background image" />
+      </div>
+
+      <div className='container'>
+        <h1>
+          Implementation av boka tvättid
+        </h1>
+      </div>
+
     </>
   )
 }
