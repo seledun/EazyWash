dca26f8aa9d78905d5aecd68d79c75c803cd1729
Sebastian Ledung <s.ledung@gmail.com>
2023-03-30 00:18:42 +0200

    Prototypredo css, fixade problem med .nav

--- eazywash/src/pages/_document.tsx
+++ eazywash/src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
         <link href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossOrigin="anonymous" async></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossOrigin="anonymous"></link>
       </Head>
       <body>
--- eazywash/src/pages/index.tsx
+++ eazywash/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/index.module.css'
+import backgroundImage from 'public/xp.png'
 
 export default function Index() {
   return (
@@ -12,35 +13,37 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossOrigin="anonymous" async></script>
       </Head>
 
       <main className={styles.main}>
           <header className='container-fluid header'>            
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
               <div className="container-fluid">
-                <a className="navbar-brand" href="#"><i className="bx bxs-washer"></i>EazyWash</a>
+                <a className="navbar-brand" href="#"><i className="bx bxs-washer nav-logo"></i>EazyWash</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
-                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                      <a className="nav-link active" aria-current="page" href="#">Boka tvättid</a>
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+                  <li className="nav-item">
+                      <a className="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">Felanmälan</a>
+                      <a className="nav-link active" aria-current="page" href="#">Boka tid</a>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">Om oss</a>
+                      <a className="nav-link active" aria-current="page" href="#">Tips</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link active" aria-current="page" href="#">Kontakta oss</a>
                     </li>
                   </ul>
                   <ul className="navbar-nav mb-2 mb-lg-0"> 
                       <li className="nav-item">
-                      <a className="nav-link" href="#"><i className="ri-user-fill"></i>Logga in</a>
+                      <a className="nav-link active" href="#"><i className="ri-user-fill"></i>Logga in</a>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">Registrera</a>
+                      <a className="nav-link active" href="#">Registrera</a>
                     </li>
                   </ul>
                 </div>
@@ -49,16 +52,19 @@ export default function Index() {
           </header>
       </main>
 
-
-      <div className="container-xl content">
-        <Image src="/xp.png" width={1000} height={1000} alt="background image" />
-      </div>
-
-      <div className='container'>
-        <h1>
-          Implementation av boka tvättid
-        </h1>
-      </div>
+      <div className="container-fluid">
+        <Image src={backgroundImage} alt="background image" />
+          <div className="tips-box">
+              <div className="divbox">
+                <h2>This is the content of the first div box</h2>
+                <p>Here is some text for the first div box</p>
+              </div>
+              <div className="divbox">
+                <h2>This is the content of the second div box</h2>
+                <p>Here is some text for the second div box</p>
+              </div>
+            </div>
+        </div>
 
     </>
   )
