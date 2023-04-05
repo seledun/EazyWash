220682ad761e1031e2a1faf581d1e3abdb87b066
Sebastian Ledung <s.ledung@gmail.com>
2023-04-05 07:12:32 +0200

    Fixade ikonerna i navbar efter design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/index.module.css'
 import backgroundImage from 'public/xp.png'
+import loginImage from 'public/user.svg'
+import washerLogo from 'public/washer.svg'
+
 
 export default function Index() {
   return (
@@ -19,7 +22,7 @@ export default function Index() {
           <header className='container-fluid header'>            
             <nav className="navbar navb navbar-dark bg-dark navbar-expand-lg">
               <div className="container-fluid">
-                <a className="navbar-brand" href="#"><i className="bx bxs-washer nav-logo"></i>EazyWash</a>
+                <a className="navbar-brand" href="#"><Image src={washerLogo} alt="Logotyp" />EazyWash</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                 </button>
@@ -40,7 +43,7 @@ export default function Index() {
                   </ul>
                   <ul className="navbar-nav mb-2 mb-lg-0"> 
                       <li className="nav-item">
-                      <a className="nav-link mx-4 my-2" href="#"><i className="ri-user-fill"></i>Logga in</a>
+                      <a className="nav-link mx-4 my-2" href="#"><Image src={loginImage} alt="Login image" />Logga in</a>
                     </li>
                     <li className="nav-item">
                       <a className="nav-link mx-4 my-2" href="#">Registrera</a>
