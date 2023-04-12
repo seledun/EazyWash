1ab0344c5d85c673c2a05598c6f4e4d14e44bf3e
Sebastian Ledung <s.ledung@gmail.com>
2023-04-12 15:29:26 +0200

    Prismakoppling
    Fixade snabb autentisering av lösenord
    
    Co-Authored-By: Gefors <112483367+Gefors@users.noreply.github.com>
    Co-Authored-By: PetterCarlsson <112485781+PetterCarlsson@users.noreply.github.com>

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client'
 
 /**
  * Login API, needs a POST-object from the user containing 
@@ -6,10 +7,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
  * 
  * @param req Request-object from the client.
  * @param res Response-object to for the client to get a response
- * @author Sebastian Ledung, an3944
+ * @author Sebastian Ledung
+ * @author Teo Gefors
+ * @author Petter Carlsson
  */
 
-export default function login(
+export default async function login(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
@@ -19,7 +22,19 @@ export default function login(
         let parsedPin = Number(pin);
 
         if (!isNaN(parsedPin) && id.length < 12) { // If pin-code is numeric && id length < 12 chars
+            const prisma = new PrismaClient()
             res.status(200).json({success: 'true'});
+
+            // const result = await prisma.$queryRaw`call add_person('test1231', '123', 2);`;
+
+            const result = await prisma.person.findFirst(id);
+            let password = result?.password;
+
+            if (password === pin) {
+                console.log("Nice! du är inne");
+            }
+
+            prisma.$disconnect;
         } 
         
         else {
