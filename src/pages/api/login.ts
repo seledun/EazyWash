a3ff1e40c0c1624d42566de10d23c3e0034cab2d
Sebastian Ledung <s.ledung@gmail.com>
2023-04-12 18:45:56 +0200

    Fixade validering av indata mot APIn
    använde mig av tabellbegränsningarna satta i postgres för att validera indatan som når API-endpointen.

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -12,6 +12,26 @@ import { PrismaClient } from '@prisma/client'
  * @author Petter Carlsson
  */
 
+/**
+ * pin can only be between 1-30 characters long
+ * @author Sebastian Ledung
+ */
+function validatePinCode(pin: string) {
+    return (pin.length > 0 && pin.length <= 30);
+}
+
+/**
+ * id can only be length > 0 and length < 50
+ * id can only be letters and numbers a-z, A-Z and 0-9
+ * @param id id to validate
+ * @author Sebastian Ledung 
+ */
+function validateUserName(id: string) {
+    let lengthValid = (id.length > 0 && id.length <= 50);
+    let charsValid = id.match(/^[A-Za-z0-9]*$/);
+    return (lengthValid && charsValid);
+}
+
 export default async function login(
     req: NextApiRequest,
     res: NextApiResponse
@@ -21,7 +41,7 @@ export default async function login(
         const {id, pin} = req.body;
         let parsedPin = Number(pin);
 
-        if (!isNaN(parsedPin) && id.length < 12) { // If pin-code is numeric && id length < 12 chars
+        if (validateUserName(id) && validatePinCode(pin)) {
             const prisma = new PrismaClient()
             res.status(200).json({success: 'true'});
 
