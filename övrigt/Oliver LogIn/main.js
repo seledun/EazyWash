44cfb7002a309f99fd1434b60d2aebfc93a447ba
PetterCarlsson <petter.carlsson98@gmail.com>
2023-04-13 15:45:53 +0200

    Början på logga in

--- övrigt/Oliver LogIn/main.js
+++ övrigt/Oliver LogIn/main.js
@@ -1,3 +1,6 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient();
+
 document.getElementsByClassName("LoggaInButton")[0].addEventListener("click",function(){
 	document.querySelector(".popup").style.display="flex";
 })
@@ -7,3 +10,15 @@ document.getElementsByClassName("btn-close")[0].addEventListener("click",functio
 	document.querySelector(".popup").style.display = "none";
 		
 })
+
+const loginButton = document.querySelector('.button');
+loginButton.addEventListener('click', function() {
+
+	const usernameInput = document.querySelector('input[type="text"]');
+  const passwordInput = document.querySelector('input[type="password"]');
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  console.log(`Username: ${username}, Password: ${password}`);
+});
