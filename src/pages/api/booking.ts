35ed97ff329d20dede4d617b40416a10229213fb
Sebastian Ledung <s.ledung@gmail.com>
2023-05-04 17:30:18 +0200

    APIn tar emot dag och tid samt läser användar-id från cookie
    Borde även implementera validering av sessions-idt

--- src/pages/api/booking.ts
+++ src/pages/api/booking.ts
@@ -1,58 +1,72 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from '../../utils/prisma';
-import { addHours } from "date-fns";
-import { cookies } from 'next/headers';
-
-// todo
+import { parse } from 'cookie';
 
 /**
- * Login API, needs a GET-object from the user containing 
- * username and a 4-number PIN.
- * 
+ * Booking API, needs a POST-object from the user containing 
+ * day and timeslot to book.
  * @param req Request-object from the client.
  * @param res Response-object to for the client to get a response
- * @author Sebastian Ledung, an3944
+ * @author Sebastian Ledung
  */
 
 type TimeSlot = {
-  startDate: Date,
-  endDate: Date
+  startDate: string,
+  endDate: string
 };
 
-function timeSlots(DATE_SELECTED: Date, timeSlot: string) : TimeSlot | undefined {
-  
+/**
+ * Formats the date to a PostgresQL allowed format 
+ * @param date the date to format
+ * @returns ISO-8601 formatted string (1995-11-01).
+ */
+function formatDateISO8601(date: Date) : string {
+  const YEAR = date.getFullYear().toString();
+  const MONTH = date.getMonth().toString().padStart(2, '0');
+  const DAY = date.getDay().toString().padStart(2, '0');
+  return YEAR + '-' + MONTH + '-' + DAY;
+}
+
+/**
+ * Forces timeslots to set timespans, doesn't let the client
+ * decide what times to book instead forces the user to book
+ * a span.
+ * @param DATE_SELECTED The supplied date to book a time.
+ * @param timeSlot Client selected time-slot.
+ * @returns Formatted strings for 
+ */
+function timeSlots(timeSlot: string) : TimeSlot | undefined {
   if (timeSlot === '1') {
     return ({
-      startDate: addHours(DATE_SELECTED, 8), // 08:00 - 12:00
-      endDate: addHours(DATE_SELECTED, 12)
+      startDate: '08:00', // 08:00 - 12:00
+      endDate: '12:00'
     });
   }
 
   else if (timeSlot === '2') {
     return ({
-      startDate: addHours(DATE_SELECTED, 12), // 12:00 - 16:00
-      endDate: addHours(DATE_SELECTED, 16)
+      startDate: '12:00', // 12:00 - 16:00
+      endDate: '16:00'
     });
   }
 
   else if (timeSlot === '3') {
     return ({
-      startDate: addHours(DATE_SELECTED, 16), // 16:00 - 20:00
-      endDate: addHours(DATE_SELECTED, 20)
+      startDate: '16:00', // 16:00 - 20:00
+      endDate: '20:00'
     });
   }
 
   else if (timeSlot === '4') {
     return ({
-      startDate: addHours(DATE_SELECTED, 20), // 20:00 - 24:00
-      endDate: addHours(DATE_SELECTED, 24)
+      startDate: '20:00', // 20:00 - 24:00
+      endDate: '24:00'
     });
   }
 
   return undefined; // If request is malformed.
 
 }
-
 
 export default async function booking(
   req: NextApiRequest,
@@ -62,21 +76,28 @@ export default async function booking(
     res.status(200).json({sucess: 'true'});
     
     const {day, timeSlot} = req.body;
-    const DATE_SELECTED = new Date(day);
 
-    const DATES = timeSlots(DATE_SELECTED, timeSlot);
-    const COOKIES = cookies().get('user-id')?.value;
+    const DATES = timeSlots(timeSlot);
 
-    if (DATES !== undefined && COOKIES !== undefined) {
-      console.log(DATES);
+    const COOKIES = parse(req.headers.cookie || '');
+    const USER_ID : number = parseInt(COOKIES['user-id']);
 
-      console.log("cookie: " + COOKIES);
-      console.log("start date: " + DATES.startDate);
-      console.log("end date: " + DATES.endDate);
+    if (DATES !== undefined && !Number.isNaN(COOKIES)) {
+      const DATE_NOW_ISO_8601 = formatDateISO8601(new Date());
+      const QUERY = "call add_booking('" 
+        + DATES.startDate 
+        + "', '" + DATES.endDate 
+        + "', " + USER_ID 
+        + ", '" + DATE_NOW_ISO_8601 
+        + "');"; 
 
-      const RESULT = await prisma.$queryRaw`call add_booking(${DATES.startDate}, ${DATES.endDate}, ${COOKIES}, ${new Date()})`;
+      prisma.$connect;
+
+      console.log(QUERY);
+      const RESULT = await prisma.$queryRawUnsafe(QUERY);
       console.log(RESULT);
-
+    
+      prisma.$disconnect;
     }
 
   } else {
